09e816f466ca143208872b3bd40e94cf32d49d57
GenXwater <louis.genty@outlook.com>
2025-02-01 11:34:26 +0100

    Add interface for Directors extend of teacher

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,24 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  yearsOfExperience: 10,
+  numberOfReports: 17,
+};
+
+console.log(director1);
